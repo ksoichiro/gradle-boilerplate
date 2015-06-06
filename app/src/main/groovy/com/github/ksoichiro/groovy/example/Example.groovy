007eb05e0b9d570a9a3095c965e298102c284bcc
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-06-06 20:46:44 +0900

    Changed tabs to spaces.

--- app/src/main/groovy/com/github/ksoichiro/groovy/example/Example.groovy
+++ app/src/main/groovy/com/github/ksoichiro/groovy/example/Example.groovy
@@ -1,20 +1,20 @@
 package com.github.ksoichiro.groovy.example
 
 class Example {
-	static main(args) {
-		def example = new Example()
+    static main(args) {
+        def example = new Example()
         example.execute()
         example.say()
-	}
+    }
 
-	def execute() {
-		def config = new ConfigSlurper().parse(
-				new File(getClass().classLoader.getResource("config.groovy").toURI()).text)
-		println "Environment: ${config.app.env}"
-	}
+    def execute() {
+        def config = new ConfigSlurper().parse(
+                new File(getClass().classLoader.getResource("config.groovy").toURI()).text)
+        println "Environment: ${config.app.env}"
+    }
 
-	def say() {
-		println 'Hello, world'
-		1
-	}
+    def say() {
+        println 'Hello, world'
+        1
+    }
 }
--- app/src/test/groovy/com/github/ksoichiro/groovy/example/ExampleSpec.groovy
+++ app/src/test/groovy/com/github/ksoichiro/groovy/example/ExampleSpec.groovy
@@ -3,16 +3,16 @@ package com.github.ksoichiro.groovy.example
 import spock.lang.Specification
 
 class ExampleSpec extends Specification {
-	def "main"() {
-		expect:
-		Example.main(null)
-	}
+    def "main"() {
+        expect:
+        Example.main(null)
+    }
 
-	def "say returns 1"() {
-		when:
-		def result = new Example().say()
+    def "say returns 1"() {
+        when:
+        def result = new Example().say()
 
-		then:
-		result == 1
-	}
+        then:
+        result == 1
+    }
 }
