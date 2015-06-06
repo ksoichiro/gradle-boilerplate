eb491142e2c78dfd967a0bddd5cc400ba8f0526d
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-06-06 20:06:01 +0900

    Added config per environment.

--- app/src/main/groovy/com/github/ksoichiro/groovy/example/Example.groovy
+++ app/src/main/groovy/com/github/ksoichiro/groovy/example/Example.groovy
@@ -2,7 +2,15 @@ package com.github.ksoichiro.groovy.example
 
 class Example {
 	static main(args) {
-		new Example().say()
+		def example = new Example()
+        example.execute()
+        example.say()
+	}
+
+	def execute() {
+		def config = new ConfigSlurper().parse(
+				new File(getClass().classLoader.getResource("config.groovy").toURI()).text)
+		println "Enviromnent: ${config.app.env}"
 	}
 
 	def say() {
